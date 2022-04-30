a731292a07f3e582024f01b89c6457642ebd0c8b
IndPrinciple <syxx2013050321@163.com>
2022-04-30 21:10:14 +0800

    9_Merge_Abs

--- models/layers/Lip_Layer.py
+++ models/layers/Lip_Layer.py
@@ -71,6 +71,49 @@ class Uni_Linear(nn.Module):
         w_times_x = torch.mm(x, w.t() / broadcasted_sumw)
         return torch.add(w_times_x, self.bias)  # w times x + b
 
+class Dist_Conv2D_Dense(nn.Module):
+    torch.nn.Conv2d
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, conn_num=None):
+        super().__init__()
+        self.in_channels, self.out_channels, self.padding, self.kernel_size, self.stride, self.conn_num =\
+            in_channels, out_channels, padding, kernel_size, stride, conn_num
+        weights = torch.Tensor(out_channels, in_channels, kernel_size, kernel_size)
+        self.weights = nn.Parameter(weights)  # nn.Parameter is a Tensor that's a module parameter.
+        bias = torch.Tensor(out_channels, 1, 1)
+        self.bias = nn.Parameter(bias)
+
+        # initialize weights and biases
+        nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5)) # weight init
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
+        bound = 1 / math.sqrt(fan_in)
+        nn.init.uniform_(self.bias, -bound, bound)  # bias init
+
+    def forward(self, x):
+
+        pad_l = self.padding // 2
+        pad_r = self.padding - pad_l
+        x_pad = nn.functional.pad(x, (pad_l, pad_r, pad_l, pad_r))
+
+        x_unfold = x_pad.unfold(-2, self.kernel_size, self.stride).unfold(-2, self.kernel_size, self.stride)
+        x_unfold = x_unfold.permute((0, 2, 3, 1, 4, 5))
+
+        B = x_unfold.shape[0]
+        Cin = self.in_channels
+        Cout = self.out_channels
+        H = x_unfold.shape[1]
+        W = x_unfold.shape[2]
+        K = self.kernel_size
+        assert(Cin == self.in_channels)
+
+        x_reshape = x_unfold.reshape((B*H*W, Cin*K*K))
+        w_reshape = self.weights.reshape((Cout, Cin*K*K))
+
+        w_diff_x = torch.cdist(x_reshape, w_reshape, p=torch.inf)
+
+        output = w_diff_x.reshape((B, H, W, Cout)).permute((0, 3, 1, 2))
+
+        return output + self.bias
+
 class Dist_Conv2D(nn.Module):
     torch.nn.Conv2d
     def __init__(self, in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=0, p=torch.inf, conn_num=None):

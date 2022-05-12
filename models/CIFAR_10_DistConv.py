e5455c833bdc9ecadf52dada7183ac7dc3e20520
IndPrinciple <syxx2013050321@163.com>
2022-05-12 15:54:11 +0800

    10_MovingAverage

--- models/CIFAR_10_DistConv.py
+++ models/CIFAR_10_DistConv.py
@@ -1,24 +1,27 @@
 import torch
 from torch import nn
-from .layers.Lip_Layer import Dist_Conv2D, Minimax_Conv2D, Uni_Linear
-from .layers.Basic_Layers import Static_Layernorm
+from .layers.Lip_Layer import Dist_Conv2D, Minimax_Conv2D, Uni_Linear, DistConv_Res
+from .layers.Basic_Layers import Static_Layernorm, MovingAverage
 
 class CIFAR_10_DistConv(nn.Module):
     def __init__(self, conn_num, p=torch.inf):
         super(CIFAR_10_DistConv, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_cnn_stack = nn.Sequential(
-            Dist_Conv2D(3, 256, padding=2, p=p, conn_num=conn_num),
-            Dist_Conv2D(256, 256, padding=2, p=p, conn_num=conn_num), Static_Layernorm(ax=(0, -1, -2)),
-            nn.MaxPool2d(2, 2),
-            Dist_Conv2D(256, 1024, padding=2, p=p, conn_num=conn_num),
-            Dist_Conv2D(1024, 1024, padding=2, p=p, conn_num=conn_num), Static_Layernorm(ax=(0, -1, -2)),
-            nn.MaxPool2d(2, 2),
-            Dist_Conv2D(1024, 512, padding=2, p=p, conn_num=2 * conn_num),
-            Dist_Conv2D(512, 256, padding=2, p=p, conn_num=2 * conn_num), Static_Layernorm(ax=(0, -1, -2)),
-            nn.MaxPool2d(2, 2),
+            DistConv_Res(3, 256, 0.2, padding=2, p=p, conn_num=conn_num), MovingAverage(ax=(0, -1, -2)),
+            DistConv_Res(256, 256, 0.2, padding=2, p=p, conn_num=conn_num), MovingAverage(ax=(0, -1, -2)),
+            nn.MaxPool2d(2, 2), MovingAverage(ax=(0, -1, -2)),
+            DistConv_Res(256, 1024, 0.2, padding=2, p=p, conn_num=conn_num), MovingAverage(ax=(0, -1, -2)),
+            DistConv_Res(1024, 1024, 0.2, padding=2, p=p, conn_num=conn_num), MovingAverage(ax=(0, -1, -2)),
+            nn.MaxPool2d(2, 2), MovingAverage(ax=(0, -1, -2)),
+            Dist_Conv2D(1024, 2048, padding=2, p=p, conn_num=conn_num), MovingAverage(),
+            Dist_Conv2D(2048, 2048, padding=2, p=p, conn_num=conn_num), MovingAverage(),
+            nn.MaxPool2d(2, 2), MovingAverage(),
+            Dist_Conv2D(2048, 2048, padding=2, p=p, conn_num=conn_num), MovingAverage(),
+            Dist_Conv2D(2048, 1024, padding=2, p=p, conn_num=2 * conn_num), MovingAverage(),
+            nn.MaxPool2d(2, 2), MovingAverage(),
             nn.Flatten(),
-            Uni_Linear(4096, 80), Static_Layernorm(), nn.ReLU(),
+            Uni_Linear(4096, 80), MovingAverage(), nn.ReLU(),
             Uni_Linear(80, 10), nn.Softmax(dim=-1)
         )
 
--- models/layers/Basic_Layers.py
+++ models/layers/Basic_Layers.py
@@ -1,6 +1,39 @@
 import torch
 from torch import nn
 
+class MovingAverage(nn.Module):
+
+    def __init__(self, gamma=0.99, lock_cnt=torch.inf, ax=None):
+        super().__init__()
+        assert gamma > 0 and gamma < 1
+        self.std = 0
+        self.avg = None
+        self.cnt = 0
+        self.locked = False
+        self.gamma = gamma
+        self.lock_cnt = lock_cnt
+        self.ax = ax
+        self.std = 1
+
+    def forward(self, x):
+        if self.ax is not None:
+            avg = torch.mean(x, axis=self.ax, keepdim=True)
+        else:
+            avg = torch.mean(x)
+
+        self.cnt += 1
+        if self.cnt > self.lock_cnt:
+            self.locked = True
+        
+        if self.locked:
+            return x - self.avg
+        else:
+            if self.avg is None:
+                self.avg = ((1 - self.gamma) * avg).detach()
+            else:
+                self.avg = ((1 - self.gamma) * avg + self.gamma * self.avg).detach()
+            return x - avg
+
 class Static_Layernorm(nn.Module):
 
     def __init__(self, gamma=0.99, lock_cnt=torch.inf, ax=None):
